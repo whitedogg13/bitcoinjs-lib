d21970e34d862cf2a2f81d979ec175927e8c5a64
junderw <junderwood@bitcoinbank.co.jp>
2019-08-30 17:57:16 +0900

    crypto

--- test/crypto.js
+++ test/crypto.js
@@ -1,23 +1,21 @@
-const { describe, it } = require('mocha')
-const assert = require('assert')
-const bcrypto = require('../src/crypto')
-
-const fixtures = require('./fixtures/crypto')
-
-describe('crypto', () => {
-  ['hash160', 'hash256', 'ripemd160', 'sha1', 'sha256'].forEach(algorithm => {
-    describe(algorithm, () => {
-      fixtures.forEach(f => {
-        const fn = bcrypto[algorithm]
-        const expected = f[algorithm]
-
-        it('returns ' + expected + ' for ' + f.hex, () => {
-          const data = Buffer.from(f.hex, 'hex')
-          const actual = fn(data).toString('hex')
-
-          assert.strictEqual(actual, expected)
-        })
-      })
-    })
-  })
-})
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+const mocha_1 = require("mocha");
+const __1 = require("..");
+const fixtures = require("./fixtures/crypto.json");
+mocha_1.describe('crypto', () => {
+    ['hash160', 'hash256', 'ripemd160', 'sha1', 'sha256'].forEach(algorithm => {
+        mocha_1.describe(algorithm, () => {
+            fixtures.forEach(f => {
+                const fn = __1.crypto[algorithm];
+                const expected = f[algorithm];
+                mocha_1.it('returns ' + expected + ' for ' + f.hex, () => {
+                    const data = Buffer.from(f.hex, 'hex');
+                    const actual = fn(data).toString('hex');
+                    assert.strictEqual(actual, expected);
+                });
+            });
+        });
+    });
+});
